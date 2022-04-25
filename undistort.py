b4ce3b0784d5a93b9f441704a6240a4097357383
HaidyRaouf <85240515+HaidyRaouf@users.noreply.github.com>
2022-04-25 04:02:58 +0200

    Update undistort.py

--- undistort.py
+++ undistort.py
@@ -2,11 +2,11 @@ def undistort(images, camera_mtx, dist_coeff):
     """
     we will use cv2.undistort to undistort
     :param images: we will assume that the input image is RGB (imread by mpimg)
-    :param mtx: it the parameter for the calibration of the camera 
-    :param dist:is a calibration parameter for the camera
+    :param camera_mtx: it the parameter for the calibration of the camera 
+    :param dist_coeff:is a calibration parameter for the camera
     :return: Undistorted image
     """
    
-    undistortedImage = cv2.undistort(images, camera_mtx, dist_coeff, None, mtx)
+    undistorted_img = cv2.undistort(images, camera_mtx, dist_coeff, None, camera_mtx)
 
     return undistorted_img
