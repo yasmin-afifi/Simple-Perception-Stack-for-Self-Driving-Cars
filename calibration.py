db5744aa8702aa3f3fb5694509426c94ee03cf24
HaidyRaouf <85240515+HaidyRaouf@users.noreply.github.com>
2022-04-25 04:01:57 +0200

    Update calibration.py

--- calibration.py
+++ calibration.py
@@ -1,6 +1,6 @@
-global camera_mtx,dist_coeff
 def camera_calibration():
-   
+    
+    global camera_mtx,dist_coeff
     # Preparing the points for the object 
     objectPoints = np.zeros((6 * 9, 3), np.float32)
     objectPoints[:, :2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
@@ -10,7 +10,7 @@ def camera_calibration():
     image_points = []
 
     # Getting the directory of all of the calibrated images
-    img = glob.glob('./camera_cal/*.jpg')
+    img = glob.glob('./camera_calibrations/*.jpg')
     images = None
 
     for indx, fname in enumerate(img):
@@ -30,4 +30,3 @@ def camera_calibration():
     ret, camera_mtx, dist_coeff, rvecs, tvecs = cv2.calibrateCamera(object_points, image_points, imageSize, None, None)
 
 camera_calibration()
-
